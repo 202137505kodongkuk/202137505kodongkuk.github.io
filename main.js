8fea4994d3c375ffb21ceb79eca35d96c539c7bb
202137505kodongkuk <126739935+202137505kodongkuk@users.noreply.github.com>
2023-05-27 15:39:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,71 @@
-let mainText = document.querySelector("h1");
+let mainText1 = document.querySelector("h1#main-text1");
+let initialScroll1 = true;
 
 window.addEventListener("scroll", function() {
   let value = window.scrollY;
   console.log("scrollY", value);
- 
 
-  if (value > 3450) {
-    mainText.style.animation = "back 2s ease-out forwards";
+  if ((value > 3450 || value < 2000) && !initialScroll1) {
+    mainText1.style.animation = "back 2s ease-out forwards";
   } else {
-    mainText.style.animation = "slide 2s ease-out";
+    mainText1.style.animation = "slide 2s ease-out";
+  }
+
+  if (initialScroll1) {
+    initialScroll1 = false;
+  }
+});
+
+let mainText2 = document.querySelector("h1#main-text2");
+let initialScroll2 = true;
+
+window.addEventListener("scroll", function() {
+  let value = window.scrollY;
+  console.log("scrollY", value);
+
+  if ((value > 4200 || value < 2800) && !initialScroll2) {
+    mainText2.style.animation = "back 2s ease-out forwards";
+  } else {
+    mainText2.style.animation = "slide 2s ease-out";
+  }
+
+  if (initialScroll2) {
+    initialScroll2 = false;
+  }
+});
+
+let mainText3 = document.querySelector("h1#main-text3");
+let initialScroll3 = true;
+
+window.addEventListener("scroll", function() {
+  let value = window.scrollY;
+  console.log("scrollY", value);
+
+  if ((value > 5000 || value < 3600) && !initialScroll3) {
+    mainText3.style.animation = "back 2s ease-out forwards";
+  } else {
+    mainText3.style.animation = "slide 2s ease-out";
+  }
+
+  if (initialScroll3) {
+    initialScroll3 = false;
+  }
+});
+
+let mainText4 = document.querySelector("h1#main-text4");
+let initialScroll4 = true;
+
+window.addEventListener("scroll", function() {
+  let value = window.scrollY;
+  console.log("scrollY", value);
+
+  if ((value > 5800 || value < 4400) && !initialScroll4) {
+    mainText4.style.animation = "back 2s ease-out forwards";
+  } else {
+    mainText4.style.animation = "slide 2s ease-out";
+  }
+
+  if (initialScroll4) {
+    initialScroll4 = false;
   }
 });
