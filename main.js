3f9d69d9974ba28e2576c2f493461b68b3bece0c
202137505kodongkuk <126739935+202137505kodongkuk@users.noreply.github.com>
2023-06-04 14:15:28 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -73,9 +73,54 @@ window.addEventListener("scroll", function() {
 });
 
 
-function goToRandomAnchor() {
+function gotoRandomAnchor() {
     var anchors = document.querySelectorAll("h1 > a");
     var randomIndex = Math.floor(Math.random() * anchors.length);
     var randomAnchor = anchors[randomIndex];
     randomAnchor.click();
   }
+
+
+
+function goToRandomLinkH(){
+    let links = [
+      "https://www.burgerking.co.kr/?NaPm=ct%3Dlh1kkzxi%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3D%7Chk%3D59cd79f43aaca02303e6d91e9527955130beac6d#/home",
+"https://www.mcdonalds.co.kr/kor/promotion/list.do",
+"https://www.lotteeatz.com/eatzMain"]
+   
+
+    let randomIndex = Math.floor(Math.random() * links.length);
+    window.open(links[randomIndex], "_blank");
+  }
+
+  function goToRandomLinkP(){
+    let links = [
+      "http://xn--2e0b622b4gcxwb8x8a.kr/?n_media=27758&n_query=%EC%98%A4%EA%B5%AC%ED%94%BC%EC%9E%90&n_rank=1&n_ad_group=grp-a001-04-000000026089223&n_ad=nad-a001-04-000000245309836&n_keyword_id=nkw-a001-04-000004383926743&n_keyword=%EC%98%A4%EA%B5%AC%ED%94%BC%EC%9E%90&n_campaign_type=4&n_contract=tct-a001-04-000000000710037&n_ad_group_type=5&NaPm=ct%3Dlifakij4%7Cci%3D0yS0002wylDyL6pNbeW%2D%7Ctr%3Dbrnd%7Chk%3D2f48bae769e4d3b181ee43942b095acef7d84a29",
+      "https://www.mrpizza.co.kr/?NaPm=ct%3Dlifabfp2%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3Dnull%7Chk%3D6826af64e5619da9fd85fb1f65a1de9b9626ad1d",
+      "https://www.pizzaalvolo.co.kr/?NaPm=ct%3Dlifacoh3%7Cci%3Dcheckout%7Ctr%3Dds%7Ctrx%3Dnull%7Chk%3Dc756d40ef2ba43751317f5148507eddd891f3ff2"
+    ]
+   
+
+    let randomIndex = Math.floor(Math.random() * links.length);
+    window.open(links[randomIndex], "_blank");
+}
+
+
+  function goToRandomLinkC(){
+    let links = ["https://www.bhc.co.kr/","https://www.kfckorea.com/","https://m.bbq.co.kr/main.asp"]
+   
+
+    let randomIndex = Math.floor(Math.random() * links.length);
+    window.open(links[randomIndex], "_blank");
+  }
+
+  function goToRandomLinkCo(){
+    let links = ["https://www.starbucks.co.kr/index.do","https://www.hollys.co.kr/","https://www.mega-mgccoffee.com/bbs/?bbs_category=3"
+      
+      ]
+   
+
+    let randomIndex = Math.floor(Math.random() * links.length);
+    window.open(links[randomIndex], "_blank");
+  }
+
